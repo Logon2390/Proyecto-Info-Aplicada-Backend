383dfef693744ca1c6c2420e01e7697fe2b81de7
Kirigaya2002 <jeremy.vegacampos@ucr.ac.cr>
2024-10-22 17:33:34 -0600

    Backend updated v2

---
+++ Backend/Custom/Utility.cs
@@ -0,0 +1,62 @@
+﻿using Backend.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Backend.Custom
+{
+    public class Utility
+    {
+        private readonly IConfiguration _config;
+        public Utility(IConfiguration config)
+        {
+
+            _config = config;
+        }
+
+        //Metodo de encriptacion de texto
+        public string encryptSHA256(string text)
+        {
+
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(text));
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
+
+        //Metodo de generacion de JWT
+        public string generateJWT(User user)
+        {
+
+            //Crear la informacion del usuario para el token
+            var userClaims = new[] {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Name, user.Username),
+                new Claim(ClaimTypes.GivenName, user.FirstName),
+                new Claim(ClaimTypes.Surname, user.LastName),
+                new Claim(ClaimTypes.Email, user.Email),
+                new Claim(ClaimTypes.DateOfBirth, user.BirthDate.ToString("yyyy-MM-dd"))
+            };
+
+            var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]!));
+            var credentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256Signature);
+
+            //Crear detalle del token
+            var jwtToken = new JwtSecurityToken(
+                claims: userClaims,
+                expires: DateTime.Now.AddMinutes(10),
+                signingCredentials: credentials
+                );
+
+            return new JwtSecurityTokenHandler().WriteToken(jwtToken);
+        }
+    }
+}
---
+++ Backend/Migrations/Document/20241022230111_Documents.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend.Migrations.Document
+{
+    /// <inheritdoc />
+    public partial class Documents : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Documents",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    owner = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    type = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    size = table.Column<int>(type: "int", nullable: false),
+                    base64 = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Documents", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Documents_Id",
+                table: "Documents",
+                column: "Id",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Documents");
+        }
+    }
+}
