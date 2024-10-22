61a8cb957dfd69435d3bd33a6f653fc12c9d55fa
Kirigaya2002 <jeremy.vegacampos@ucr.ac.cr>
2024-10-21 19:03:00 -0600

    Estructura inicial backend

---
+++ Backend/Controllers/UsersController.cs
@@ -0,0 +1,26 @@
+﻿using Backend.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly UserContext _context;
+
+        public UsersController(UserContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        [Route("register")]
+        public async Task<IActionResult> Register(User user)
+        {
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+    }
+}
