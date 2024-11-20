777f03593cda0eba26807f4951327124881a6378
Jeremy Vega <jeremy.vegacampos@ucr.ac.cr>
2024-11-20 10:55:41 -0600

    Añadir clase RelationDocumentBase64 en Backend.Models
    Se ha añadido una nueva clase llamada `RelationDocumentBase64` en el archivo `RelationDocumentBase64.cs`. Esta clase se encuentra dentro del espacio de nombres `Backend.Models` y tiene dos propiedades: `DocumentId` de tipo `int` y `Base64` de tipo `string` y es requerida.

---
+++ Backend/Models/RelationDocumentBase64.cs
@@ -0,0 +1,8 @@
+﻿namespace Backend.Models
+{
+    public class RelationDocumentBase64
+    {
+        public int DocumentId { get; set; }
+        public required string Base64 { get; set; }
+    }
+}
